340aaadeb889c021f7d20a69143c548246da9080
Jamie Smith <jamiesmith.exe@gmail.com>
2026-03-12 10:38:05 +0000

    flowFlag decalred

--- i2c_read_write.ino
+++ i2c_read_write.ino
@@ -15,12 +15,11 @@
 const uint32_t I2C_FREQ = 400000;
 
 // I2C addresses
-#define SLF3S4000B_ADDR       0x08
+#define SLF3S4000B_ADDR 0x08
 
 bool ack;
 
-
-SLF3S4000B  flowSensor(SLF3S4000B_ADDR);
+SLF3S4000B flowSensor(SLF3S4000B_ADDR);
 
 // ------------ Setup ------------
 
@@ -35,34 +34,35 @@ void setup() {
   ack = flowSensor.startWater();
   if (ack) {
     Serial.println("Address Ack");
-    delay(50);
   } else {
     Serial.println("Address Failed");
   }
-  delay(1000);
+
+  Serial.println("Press ENTER to take a measurement");
 }
 
 // ------------ Loop ------------
+
 void loop() {
-  // Run all I2C commands once
-  static bool ranOnce = false;
-  if (ranOnce) {
-    // Stop forever after the first pass
-    Serial.println("Finished");
-    while (true) {
-      delay(1000);
-    }
+
+  // Wait for user to press ENTER
+  if (Serial.available()) {
+
+    // Clear the buffer
+    while (Serial.available()) Serial.read();
+
+    float flow = NAN, flowT = NAN, byte1 = NAN, byte2 = NAN, CRC = NAN;
+    uint16_t flowFlags = 0;
+
+    flowSensor.read(flow, flowT, flowFlags, byte1, byte2, CRC);
+
+    Serial.print("Flow: "); Serial.print(flow, 4);
+    Serial.print("  B1: "); Serial.print(byte1, HEX);
+    Serial.print("  B2: "); Serial.print(byte2, HEX);
+    Serial.print("  CRC: "); Serial.print(CRC, HEX);
+    Serial.println();
+
+    Serial.println("Press ENTER to run again");
   }
-  ranOnce = true;
 
-  float flow = NAN, flowT = NAN, byte1 = NAN, byte2 = NAN, CRC = NAN;
-  flowSensor.read(flow, flowT, flowFlags, byte1, byte2, CRC);
-  Serial.print("Flow: "); Serial.print(flow, 4);
-  Serial.print("  B1: "); Serial.print(byte1, HEX); Serial.print("  B2: "); Serial.print(byte2, HEX); Serial.print("  CRC: "); Serial.print(CRC, HEX);
-
-  Serial.print("   ");
-  Serial.println();
-
-
-  delay(100);
 }
